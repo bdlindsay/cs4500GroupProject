4b0ffaf4e60976685f05b0118e1d171cd571f481
JaseP88 <jtpbyd@mail.umsl.edu>
2016-04-17 18:11:04 -0500

    Update SlideShow.js

--- SlideShow.js
+++ SlideShow.js
@@ -186,6 +186,7 @@ function beginPlaying(gameModeChoice){
 	$("#optionsSymDiv").show(DURATION_PER_IMAGE);
 	audio.play();
 	document.getElementById("pausesText").style.visibility = "visible"; //shows the pause counter text
+	updatePauses();
 }
 
 function playActionAudio() {
